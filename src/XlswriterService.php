91e90ac3ab19cf8c5b71cce3ea3ded75f0d8af16
Jenson.Wang <rebing0512@aliyun.com>
2024-02-28 20:00:13 +0800

    1.Excel编号处理

--- src/XlswriterService.php
+++ src/XlswriterService.php
@@ -52,7 +52,7 @@ class XlswriterService
             throw new \Exception('The title is not empty!');
         }
         $defaultSetting = [
-            'fileName'=>'excel_export_'.date('Y-m-d H:i:s').'.xlsx',
+            'fileName'=>'excel_export_'.date('Y-m-d').'.xlsx',
 
             'hasSheetTitle'=>true, # 是否有表格的表头
 
@@ -96,6 +96,118 @@ class XlswriterService
                 ['Gym',  50],
             ])
             ->output();
+    }
+
+    /**
+     * 数字续号对应的字母
+     *
+     * @var string[]
+     */
+    private static $keyArr = [
+        1 => 'A',
+        2 => 'B',
+        3 => 'C',
+        4 => 'D',
+        5 => 'E',
+        6 => 'F',
+        7 => 'G',
+        8 => 'H',
+        9 => 'I',
+        10 => 'J',
+        11 => 'K',
+        12 => 'L',
+        13 => 'M',
+        14 => 'N',
+        15 => 'O',
+        16 => 'P',
+        17 => 'Q',
+        18 => 'R',
+        19 => 'S',
+        20 => 'T',
+        21 => 'U',
+        22 => 'V',
+        23 => 'W',
+        24 => 'X',
+        25 => 'Y',
+        26 => 'Z',
+    ];
+
+    /**
+     * 字母对应的数字续号
+     *
+     * @var int[]
+     */
+    private static $keyArrToNum = [
+        'A' => 1,
+        'B' => 2,
+        'C' => 3,
+        'D' => 4,
+        'E' => 5,
+        'F' => 6,
+        'G' => 7,
+        'H' => 8,
+        'I' => 9,
+        'J' => 10,
+        'K' => 11,
+        'L' => 12,
+        'M' => 13,
+        'N' => 14,
+        'O' => 15,
+        'P' => 16,
+        'Q' => 17,
+        'R' => 18,
+        'S' => 19,
+        'T' => 20,
+        'U' => 21,
+        'V' => 22,
+        'W' => 23,
+        'X' => 24,
+        'Y' => 25,
+        'Z' => 26,
+    ];
+
+    /**
+     * 获取列的字母编号
+     *
+     * @param $str
+     * @return float|int
+     */
+    public function getStrToNum($str){
+        $str = strtoupper($str);
+        $len = strlen($str);
+        $num = 0;
+        for($i=0;$i<$len;$i++){
+            $num += static::$keyArrToNum[substr($str,$i,1)]*pow(26,$len-1-$i);
+        }
+        return $num;
+    }
+
+    /**
+     * 获取列的字母编号
+     *
+     * @param $num
+     * @return false|string
+     */
+    public function getStr($num){
+        #如果不是整数，或小于0
+        if(!is_int($num) || $num<=0){
+            return false;
+        }else if($num<=26){
+            return self::$keyArr[$num];
+        }else{
+            #取余数
+            $num2 = $num%26;
+            if($num2==0){
+                $num2 = 26;
+                #取整数
+                $num = intval(floor(($num-26)/26));
+                return self::getStr($num).self::getStr($num2);
+            }else{
+                #取整数
+                $num = intval(floor($num/26));
+                return self::getStr($num).self::getStr($num2);
+            }
+        }
     }
 
 }
